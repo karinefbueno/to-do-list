b4c2a471a8f9e8d89a5abdd3a8a2d9bb091bd226
Karine <karine.fbueno@gmail.com>
2024-07-02 17:22:55 -0300

    C03-feat: component Form

---
+++ frontend/src/components/Form/index.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ContainerForm, ContainerInput, FormComponent, Input, MessageError } from "./style";
+import { handleChange } from "../../utils/handleChange";
+import { validateFormData } from "../../utils/validateForm";
+
+type FormProps = {
+  input?: React.ReactNode,
+  buttonLogin?: React.ReactNode,
+  button?: React.ReactNode,
+  isLoginForm?: boolean,
+  onSubmit?: (e: React.FormEvent<HTMLFormElement>, formData: typeof INITIAL_VALUE) => void,
+};
+
+const INITIAL_VALUE = {
+  email: '',
+  password: '',
+  name: '',
+};
+
+const Form: React.FC<FormProps> = ({ input, buttonLogin, button, onSubmit }) => {
+  const [formData, setFormData] = useState(INITIAL_VALUE);
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const errors = validateFormData(formData);
+    if (errors.email) {
+      setEmailError(errors.email);
+    } else {
+      setEmailError('');
+    }
+    if (errors.password) {
+      setPasswordError(errors.password);
+    } else {
+      setPasswordError('');
+    }
+
+    if (!errors.email && !errors.password) {
+      if (onSubmit) {
+        onSubmit(e, formData);
+      }
+      setFormData(INITIAL_VALUE);
+      navigate('/profile')
+
+    }
+  };
+
+  return (
+    <ContainerForm>
+      <FormComponent onSubmit={handleSubmit}>
+        {input}
+        <ContainerInput>
+        <label htmlFor="email">
+          <Input
+            name="email"
+            onChange={handleChange(setFormData)}
+            value={formData.email}
+            type="email"
+            placeholder="E-mail"
+          />
+        </label>
+        {emailError && 
+        <MessageError>{emailError}</MessageError>}
+        </ContainerInput>
+        <ContainerInput>
+        <label htmlFor="password">
+          <Input
+            name="password"
+            onChange={handleChange(setFormData)}
+            value={formData.password}
+            type="password"
+            placeholder="Senha"
+            minLength={6}
+          />
+        </label>
+        {passwordError && 
+        <MessageError>{passwordError}</MessageError>
+        }
+         </ContainerInput>
+
+        {buttonLogin}
+        {button}
+      </FormComponent>
+    </ContainerForm>
+  );
+};
+
+export default Form;
