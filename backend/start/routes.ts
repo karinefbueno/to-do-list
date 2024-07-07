8b4b63cd10c3a48b682d4b819c46dc29dd555604
Karine <karine.fbueno@gmail.com>
2024-07-07 12:32:31 -0300

    C10- user routes

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -6,11 +6,18 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
+// import type { HttpContext } from '@adonisjs/core/http'
 import router from '@adonisjs/core/services/router'
+const UsersController = () => import('#controllers/users_controller')
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.post('/users', [UsersController, 'create'])
+router.delete('/users/:id', [UsersController, 'delete'])
+router.put('/users/:id', [UsersController, 'update'])
+router.get('users', [UsersController, 'getAll'])
+router.get('users/:id', [UsersController, 'getById'])
