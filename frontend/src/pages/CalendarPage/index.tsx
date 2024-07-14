45802b09bfa7b9b7e4c41c6706292d32fc99be3f
Karine <karine.fbueno@gmail.com>
2024-07-14 15:41:43 -0300

    C05-Calender page

---
+++ frontend/src/pages/CalendarPage/index.tsx
@@ -0,0 +1,12 @@
+import CalendarComponent from '../../components/Calendar ';
+
+function CalendarPage() {
+	return (
+		<>
+			<h1>Calendar</h1>
+			<CalendarComponent />
+		</>
+	);
+}
+
+export default CalendarPage;
