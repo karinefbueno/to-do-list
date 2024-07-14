e66067380747582a4ea18dadd49f4dd047b4bfa1
Karine <karine.fbueno@gmail.com>
2024-07-14 16:11:49 -0300

    C12- global style

---
+++ frontend/src/style/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+     font-family: 'Cutive', serif;
+    font-weight: 400; 
+    font-style: normal; 
+  }
+
+  :focus {
+    outline: none;
+  }
+
+  body {
+    height: 100%;
+    /* background-color: rgba(1, 13, 17); */
+   
+  }
+`;
