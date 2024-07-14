1c6a2a981cb2e66dcb4d62826bce5448219ed867
Karine <karine.fbueno@gmail.com>
2024-07-14 16:09:57 -0300

    C11- ajustes main e app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import CalendarComponent from './components/Calendar ';
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path='/' element={<Login />} />
-				<Route path='/profile ' element={<Profile />} />
+				<Route path='/profile' element={<Profile />} />
+				<Route path='/calendar' element={<CalendarComponent />} />
 			</Routes>
 		</>
 	);
